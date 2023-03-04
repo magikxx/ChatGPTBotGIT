100f4d76ae5bf9532630492a88510b93bfb15348
ImprezaXZ <imprezaxluv@gmail.com>
2023-03-04 23:14:45 +0200

    removed code-davinci mode

--- bot.py
+++ bot.py
@@ -32,20 +32,11 @@ def message_handler(message):
 def callback_query(call):
     if call.data == "cb_english":
         bot.send_message(call.message.chat.id, 'Hello! Im ChatGPT telegram bot\n' 'Im using GPT-3.5 engine\n'
-                     '\nGeneral model is text-davinci-003 model\n'
-                     '\nFor coding optimize add /code to your question, this will provide you an code-optimized answer, using code-davinci-002 model\n'
-                     '\nExample:'
-                     '\n/code How to write a function in python'
-                     '\nHow are you ?\n'
-                     '\nAttention, code_davinci-002 in beta stage, and have low rate limit, which causing no response and bot shutdown')
+                     '\nUsing a text-davinci-003 model\n')
     elif call.data == "cb_ukrainian":
         bot.send_message(call.message.chat.id, "Привіт! Я ChatGPT telegram бот\n" 'Я використовую модель відповідей GPT-3.5\n' 
-                         '\nЗагальні питання обробляються моделью text-davinci-003\n'
-                         "\nДля отримання код-оптимізованих відповідей, додайте до свого питання /code, це питання буде обробленне моделью code-davinci-002\n"
-                         '\nНаприклад:'
-                         '\n/code Як написати фукнції на python'
-                         '\nЯк твої справи?\n'
-                         '\nУвага, code-davinci-002 знаходиться на стадії бета та має маленький rate limit, що може залишити Вас без відповіді та закрити бота:(')
+                         '\nПитання обробляються моделью text-davinci-003\n')
+
         
 @bot.message_handler(func=lambda message: True)
 def general_text(message):
@@ -59,16 +50,5 @@ def general_text(message):
 
     bot.reply_to(message, general_text)
     
-@bot.message_handler(commands=['code'], func=lambda message: True)
-def code_text(message):
-    response = openai.Completion.create(
-        engine=code_engine,
-        prompt=message.text,
-        max_tokens=max_tokens,
-        temperature=temperature
-)
-    code_text = response.choices[0].text
-
-    bot.reply_to(message, code_text)
 
 bot.polling()
